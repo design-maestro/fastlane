8515789b191c9bc0d21ede65a50eaea31052bf6b
ShlyapaVkepke <276580998+ShlyapaVkepke@users.noreply.github.com>
2026-09-17 16:01:21 +0300

    Fix static AWG tool packaging

--- scripts/build-amneziawg-tools.sh
+++ scripts/build-amneziawg-tools.sh
@@ -7,9 +7,11 @@ AWG_CC="${AWG_CC:-cc}"
 AWG_TOOLS_COMMIT="ee0f0a9aa34ff0a0da4b3433b9512781cfe02843"
 AWG_TOOLS_SHA256="22438f231d39ea27e4bdc69707ec3103357f3cecc9ad3eecba18a61e5b39c9b9"
 AWG_TOOLS_VERSION="3.1.20260812"
-build_cflags="-O2 -static -D_GNU_SOURCE -D'WIREGUARD_TOOLS_VERSION=\"${AWG_TOOLS_VERSION}\"'"
+build_cflags="-O2 -D_GNU_SOURCE -D'WIREGUARD_TOOLS_VERSION=\"${AWG_TOOLS_VERSION}\"'"
+build_ldflags="-static"
 if [ "$(uname -s)" = "Darwin" ]; then
 	build_cflags="-O2 -D_GNU_SOURCE -D'WIREGUARD_TOOLS_VERSION=\"${AWG_TOOLS_VERSION}\"'"
+	build_ldflags=""
 fi
 
 mkdir -p "${OUTPUT_DIR}"
@@ -36,7 +38,7 @@ source_dir="${work_dir}/amneziawg-tools-${AWG_TOOLS_COMMIT}/src"
 
 (
 	cd "${source_dir}"
-	CC="${AWG_CC}" PLATFORM=linux CFLAGS="${build_cflags}" make
+	CC="${AWG_CC}" PLATFORM=linux CFLAGS="${build_cflags}" LDFLAGS="${build_ldflags}" make
 )
 install -m 0755 "${source_dir}/wg" "${OUTPUT_DIR}/amneziawg"
 install -m 0644 "${work_dir}/amneziawg-tools-${AWG_TOOLS_COMMIT}/COPYING" "${OUTPUT_DIR}/amneziawg-tools-COPYING"
